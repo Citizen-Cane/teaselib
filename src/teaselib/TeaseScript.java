084371f5bcd7ca5a469ade81d7ab5f3a5f348dce
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2014-12-12 20:55:04 +0100

    choose returns selected object

--- src/teaselib/TeaseScript.java
+++ src/teaselib/TeaseScript.java
@@ -1,5 +1,7 @@
 package teaselib;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import teaselib.audio.RenderSound;
@@ -14,188 +16,193 @@ import teaselib.util.RenderDesktopItem;
 
 public abstract class TeaseScript extends TeaseScriptBase {
 
-	public static final int Yes = 0;
-	public static final int No = 1;
-	public static final int Timeout = -1;
+    public static final int NoTimeout = 0;
 
-	public static final int NoTimeout = 0;
+    public ImageIterator dominantImages = null;
+    private String displayImage = DominantImage;
+    public final static String NoImage = "NoImage";
+    public final static String DominantImage = "DominantImage";
 
-	public ImageIterator dominantImages = null;
-	private String displayImage = DominantImage;
-	public final static String NoImage = "NoImage";
-	public final static String DominantImage = "DominantImage";
+    private String attitude = Attitude.Neutral;
 
-	private String attitude = Attitude.Neutral;
+    public final Actor actor;
 
-	public final Actor actor;
+    public TeaseScript(TeaseLib teaseLib, String locale) {
+        this(teaseLib, new Actor("Dominant", locale));
+    }
 
-	public TeaseScript(TeaseLib teaseLib, String locale) {
-		this(teaseLib, new Actor("Dominant", locale));
-	}
+    public TeaseScript(TeaseLib teaseLib, Actor actor) {
+        super(teaseLib, actor.locale);
+        this.actor = actor;
+    }
 
-	public TeaseScript(TeaseLib teaseLib, Actor actor) {
-		super(teaseLib, actor.locale);
-		this.actor = actor;
-	}
+    /**
+     * Return a random number
+     * 
+     * @param min
+     * @param max
+     * @return A value in the interval [min, max]
+     */
+    public int getRandom(int min, int max) {
+        return teaseLib.host.getRandom(min, max);
+    }
 
-	/**
-	 * Return a random number
-	 * 
-	 * @param min
-	 * @param max
-	 * @return A value in the interval [min, max]
-	 */
-	public int getRandom(int min, int max) {
-		return teaseLib.host.getRandom(min, max);
-	}
+    /**
+     * Renders the image denoted by the path. The image will not be displayed
+     * immediately but during the next message rendering. This is because if no
+     * image is specified, an image of the dominant character will be used.
+     * 
+     * @param path
+     *            The path to the image
+     */
+    public void showImage(String path) {
+        if (path == DominantImage) {
+            displayImage = DominantImage;
+            // Render dominant image with the next message
+        } else if (path == NoImage) {
+            displayImage = NoImage;
+            deferredRenderers.add(RenderNoImage.instance);
+        } else {
+            displayImage = path;
+            deferredRenderers.add(new RenderImage(path));
+        }
+    }
 
-	/**
-	 * Renders the image denoted by the path. The image will not be displayed
-	 * immediately but during the next message rendering. This is because if no
-	 * image is specified, an image of the dominant character will be used.
-	 * 
-	 * @param path
-	 *            The path to the image
-	 */
-	public void showImage(String path) {
-		if (path == DominantImage) {
-			displayImage = DominantImage;
-			// Render dominant image with the next message
-		} else if (path == NoImage) {
-			displayImage = NoImage;
-			deferredRenderers.add(RenderNoImage.instance);
-		} else {
-			displayImage = path;
-			deferredRenderers.add(new RenderImage(path));
-		}
-	}
+    public void showDesktopItem(String path) {
+        MediaRenderer desktopItem = new RenderDesktopItem(path);
+        deferredRenderers.add(desktopItem);
+    }
 
-	public void showDesktopItem(String path) {
-		MediaRenderer desktopItem = new RenderDesktopItem(path);
-		deferredRenderers.add(desktopItem);
-	}
+    public void playSound(String path) {
+        deferredRenderers.add(new RenderSound(path));
+    }
 
-	public void playSound(String path) {
-		deferredRenderers.add(new RenderSound(path));
-	}
+    void setAttitude(String attitude) {
+        this.attitude = attitude;
+    }
 
-	void setAttitude(String attitude) {
-		this.attitude = attitude;
-	}
+    /**
+     * Wait the requested numbers of seconds after displaying a message.
+     * 
+     * @param seconds
+     *            How long to wait.
+     */
+    public void delay(int seconds) {
+        deferredRenderers.add(new RenderDelay(seconds));
+    }
 
-	/**
-	 * Wait the requested numbers of seconds after displaying a message.
-	 * 
-	 * @param seconds
-	 *            How long to wait.
-	 */
-	public void delay(int seconds) {
-		deferredRenderers.add(new RenderDelay(seconds));
-	}
+    public void say(String text) {
+        Message message = new Message(actor);
+        if (text != null) {
+            message.add(text);
+        }
+        say(message);
+    }
 
-	public void say(String text) {
-		Message message = new Message(actor);
-		if (text != null) {
-			message.add(text);
-		}
-		say(message);
-	}
+    public void say(String... message) {
+        say(new Message(actor, message));
+    }
 
-	public void say(String... message) {
-		say(new Message(actor, message));
-	}
+    public void say(Message message) {
+        renderMessage(message, speechSynthesizer);
+    }
 
-	public void say(Message message) {
-		renderMessage(message, speechSynthesizer);
-	}
+    /**
+     * Show instructional text, this is not spoken, just displayed
+     * 
+     * @param message
+     *            The text top be displayed
+     */
+    public void show(String message) {
+        renderMessage(new Message(actor, message), null);
+    }
 
-	/**
-	 * Show instructional text, this is not spoken, just displayed
-	 * 
-	 * @param message
-	 *            The text top be displayed
-	 */
-	public void show(String message) {
-		renderMessage(new Message(actor, message), null);
-	}
+    protected void renderMessage(Message message,
+            TextToSpeechPlayer speechSynthesizer) {
+        try {
+            renderMessage(message, speechSynthesizer,
+                    displayImage == DominantImage ? dominantImages : null,
+                    attitude);
+        } finally {
+            displayImage = DominantImage;
+            attitude = Attitude.Neutral;
+        }
+    }
 
-	protected void renderMessage(Message message,
-			TextToSpeechPlayer speechSynthesizer) {
-		try {
-			renderMessage(message, speechSynthesizer,
-					displayImage == DominantImage ? dominantImages : null, attitude);
-		} finally {
-			displayImage = DominantImage;
-			attitude = Attitude.Neutral;
-		}
-	}
+    /**
+     * Displays the requested choices in the user interface after the mandatory
+     * parts of all renderers have been completed. This means especially that
+     * all text has been displayed and spoken.
+     * 
+     * @param choices
+     * @return
+     */
+    public String choose(List<String> choices) {
+        return choose(NoTimeout, choices);
+    }
 
-	/**
-	 * Displays the requested choices in the user interface after the mandatory
-	 * parts of all renderers have been completed. This means especially that
-	 * all text has been displayed and spoken.
-	 * 
-	 * @param choices
-	 * @return
-	 */
-	public int choose(List<String> choices) {
-		return choose(choices, TeaseScript.NoTimeout);
-	}
+    /**
+     * Display choices. Won't wait for mandatory parts to complete.
+     * 
+     * @param choices
+     * @param timeout
+     *            The timeout for the button set in seconds, or 0 if no timeout
+     *            is desired
+     * @return The button index, or TeaseLib.None if the buttons timed out
+     */
+    public String choose(int timeout, final List<String> choices) {
+        completeMandatory();
+        return showChoices(timeout > NoTimeout ? () -> {
+            teaseLib.host.sleep(timeout * 1000);
+        } : null, choices);
+    }
 
-	/**
-	 * Display choices. Won't wait for mandatory parts to complete.
-	 * 
-	 * @param choices
-	 * @param timeout
-	 *            The timeout for the button set in seconds, or 0 if no timeout
-	 *            is desired
-	 * @return The button index, or TeaseLib.None if the buttons timed out
-	 */
-	public int choose(final List<String> choices, int timeout) {
-		completeMandatory();
-		return showChoices(choices, timeout > 0 ? () -> {
-			teaseLib.host.sleep(timeout * 1000);
-		} : null);
-	}
+    /**
+     * @param choices
+     * @param timeout
+     *            The timeout for the button set in seconds, or
+     *            TeaseScript.NoTimeout if no timeout is desired
+     * @param scriptFunction
+     * @return
+     */
+    public String choose(Runnable scriptFunction, List<String> choices) {
+        // To display buttons and to start scriptFunction at the same time,
+        // completeAll() has to be called
+        // in advance in order to finish all previous render commands,
+        completeAll();
+        String choice = showChoices(scriptFunction, choices);
+        if (choice == Timeout) {
+            renderQueue.completeAll();
+        } else {
+            renderQueue.endAll();
+        }
+        return choice;
+    }
 
-	/**
-	 * @param choices
-	 * @param timeout
-	 *            The timeout for the button set in seconds, or
-	 *            TeaseScript.NoTimeout if no timeout is desired
-	 * @param scriptFunction
-	 * @return
-	 */
-	public int choose(List<String> choices, Runnable scriptFunction) {
-		// To display buttons and to start scriptFunction at the same time,
-		// completeAll() has to be called
-		// in advance in order to finish all previous render commands,
-		completeAll();
-		int choice = showChoices(choices, scriptFunction);
-		if (choice == Timeout) {
-			renderQueue.completeAll();
-		} else {
-			renderQueue.endAll();
-		}
-		return choice;
-	}
+    public String choose(String... choices) {
+        return choose(NoTimeout, new ArrayList<String>(Arrays.asList(choices)));
+    }
 
+    public String choose(Runnable scriptFunction, String... choices) {
+        return choose(scriptFunction,
+                new ArrayList<String>(Arrays.asList(choices)));
+    }
 
-	/**
-	 * Display an array of checkboxes to set or unset
-	 * 
-	 * @param caption
-	 *            The caption of the checkbox area
-	 * @param choices
-	 *            The labels of the check boxes
-	 * @param values
-	 *            Indicates whether each item is set or unset by setting the
-	 *            corresponding index to false or true.
-	 * @return
-	 */
-	public List<Boolean> showCheckboxes(String caption, List<String> choices,
-			List<Boolean> values) {
-		return teaseLib.host.showCheckboxes(caption, choices, values);
-	}
+    /**
+     * Display an array of checkboxes to set or unset
+     * 
+     * @param caption
+     *            The caption of the checkbox area
+     * @param choices
+     *            The labels of the check boxes
+     * @param values
+     *            Indicates whether each item is set or unset by setting the
+     *            corresponding index to false or true.
+     * @return
+     */
+    public List<Boolean> showCheckboxes(String caption, List<String> choices,
+            List<Boolean> values) {
+        return teaseLib.host.showCheckboxes(caption, choices, values);
+    }
 
 }
--- src/teaselib/TeaseScriptBase.java
+++ src/teaselib/TeaseScriptBase.java
@@ -28,179 +28,189 @@ import teaselib.util.Event;
 
 public abstract class TeaseScriptBase {
 
-	public final TeaseLib teaseLib;
+    public final TeaseLib teaseLib;
 
-	protected final TextToSpeechPlayer speechSynthesizer;
-	protected final SpeechRecognition speechRecognizer;
+    protected final TextToSpeechPlayer speechSynthesizer;
+    protected final SpeechRecognition speechRecognizer;
 
-	protected final MediaRendererQueue renderQueue = new MediaRendererQueue();
-	protected final Deque<MediaRenderer> deferredRenderers = new ArrayDeque<MediaRenderer>();
+    protected final MediaRendererQueue renderQueue = new MediaRendererQueue();
+    protected final Deque<MediaRenderer> deferredRenderers = new ArrayDeque<MediaRenderer>();
 
-	public TeaseScriptBase(TeaseLib teaseLib, String locale) {
-		this.teaseLib = teaseLib;
-		speechSynthesizer = new TextToSpeechPlayer(teaseLib.resources,
-				new TextToSpeech());
-		speechRecognizer = new SpeechRecognition(locale);
-	}
+    public static final String Timeout = "Timeout";
 
-	/**
-	 * Just wait for everything to be rendered (messages displayed, sounds
-	 * played, delay expired), and continue execution of the script. This won't
-	 * display a button, it just waits.
-	 */
-	public void completeAll() {
-		renderQueue.completeAll();
-	}
+    public TeaseScriptBase(TeaseLib teaseLib, String locale) {
+        this.teaseLib = teaseLib;
+        speechSynthesizer = new TextToSpeechPlayer(teaseLib.resources,
+                new TextToSpeech());
+        speechRecognizer = new SpeechRecognition(locale);
+    }
 
-	/**
-	 * Workaround as of now because PCMPlayer must display the stop button
-	 * immediately
-	 */
-	public void completeMandatory() {
-		renderQueue.completeMandatories();
-	}
+    /**
+     * Just wait for everything to be rendered (messages displayed, sounds
+     * played, delay expired), and continue execution of the script. This won't
+     * display a button, it just waits.
+     */
+    public void completeAll() {
+        renderQueue.completeAll();
+    }
 
-	public void renderMessage(Message message,
-			TextToSpeechPlayer speechSynthesizer, ImageIterator dominantImages,
-			String attitude) {
-		completeAll();
-		renderQueue.start(deferredRenderers, teaseLib);
-		deferredRenderers.clear();
-		Set<String> hints = new HashSet<>();
-		// Within messages, images might change fast, and changing
-		// the camera position, image size or aspect would be too distracting
-		hints.add(ImageIterator.SameCameraPosition);
-		hints.add(ImageIterator.SameResolution);
-		hints.add(attitude);
-		RenderMessage renderMessage = new RenderMessage(message,
-				speechSynthesizer, dominantImages, hints);
-		renderQueue.start(renderMessage, teaseLib);
-	}
+    /**
+     * Workaround as of now because PCMPlayer must display the stop button
+     * immediately
+     */
+    public void completeMandatory() {
+        renderQueue.completeMandatories();
+    }
 
-	public int showChoices(final List<String> choices, Runnable scriptFunction) {
-		// arguments check
-		for (String choice : choices) {
-			if (choice == null) {
-				throw new IllegalArgumentException("Choice may not be null");
-			}
-		}
-		TeaseLib.log("choose: " + choices.toString());
-		// Speech recognition
-		List<Integer> srChoice = new ArrayList<>(1);
-		Event<SpeechRecognitionImplementation, SpeechRecognizedEventArgs> speechRecognizedEvent = new Event<SpeechRecognitionImplementation, SpeechRecognizedEventArgs>() {
-			@Override
-			public void run(SpeechRecognitionImplementation sender,
-					SpeechRecognizedEventArgs eventArgs) {
-				if (eventArgs.result.length == 1) {
-					List<Delegate> uiElements = teaseLib.host
-							.getClickableChoices(choices);
-					// Click the button
-					SpeechRecognitionResult speechRecognitionResult = eventArgs.result[0];
-					if (speechRecognitionResult.isChoice(choices)) {
-						// This assigns the result even if the buttons have
-						// unrealized
-						int choice = speechRecognitionResult.index;
-						srChoice.add(choice);
-						try {
-							Delegate delegate = uiElements.get(choice);
-							if (delegate != null) {
-								delegate.run();
-							} else {
-								TeaseLib.log("Button gone for choice " + choice
-										+ ": " + speechRecognitionResult.text);
-							}
-						} catch (Throwable t) {
-							TeaseLib.log(this, t);
-						}
-					}
-				} else {
-					// TODO none or more than one result means incorrect
-					// recognition
-				}
-			}
-		};
-		speechRecognizer.events.recognitionCompleted.add(speechRecognizedEvent);
-		speechRecognizer.startRecognition(choices);
-		// Script closure
-		FutureTask<Integer> scriptTask = scriptFunction == null ? null
-				: new FutureTask<>(new Callable<Integer>() {
-					@Override
-					public Integer call() throws Exception {
-						try {
-							scriptFunction.run();
-						} catch (ScriptInterruptedException e) {
-							return null;
-						}
-						List<Delegate> clickables = teaseLib.host
-								.getClickableChoices(choices);
-						if (!clickables.isEmpty()) {
-							Delegate clickable = clickables.get(0);
-							if (clickable != null) {
-								clickables.get(0).run();
-							} else {
-								// Means that the host implementation is
-								// incomplete
-								new IllegalStateException(
-										"Host didn't return clickables for choices: "
-												+ choices.toString());
-							}
-						}
-						return new Integer(TeaseScript.Timeout);
-					}
-				});
-		int choice;
-		try {
-			if (scriptTask != null) {
-				ExecutorService executor = Executors.newFixedThreadPool(1);
-				executor.execute(scriptTask);
-				renderQueue.completeStarts();
-				// TODO completeStarts() doesn't work because first we need to
-				// wait for render threads that can be completed
-				// Workaround: A bit unsatisfying, but otherwise the choice
-				// buttons would appear too early
-				teaseLib.host.sleep(300);
-			}
-			choice = teaseLib.host.choose(choices);
-			if (scriptTask != null) {
-				scriptTask.cancel(true);
-			}
-			speechRecognizer.completeSpeechRecognitionInProgress();
-		} finally {
-			speechRecognizer.stopRecognition();
-			speechRecognizer.events.recognitionCompleted
-					.remove(speechRecognizedEvent);
-		}
-		// Assign result from speech recognition, tasks or button click
-		if (!srChoice.isEmpty()) {
-			choice = srChoice.get(0);
-		} else {
-			Integer r = null;
-			// Script function completed?
-			if (scriptTask != null) {
-				if (!scriptTask.isCancelled()) {
-					try {
-						// Wait for script task completion
-						r = scriptTask.get();
-					} catch (InterruptedException e) {
-						throw new ScriptInterruptedException();
-					} catch (ExecutionException e) {
-						Throwable cause = e.getCause();
-						if (cause != null) {
-							// Forward error from closure to main thread
-							throw new RuntimeException(cause);
-						} else {
-							TeaseLib.log(this, e);
-						}
-					}
-				}
-			}
-			if (r != null) {
-				choice = r;
-			} else {
-				// User clicked button, choice already assigned
-			}
-		}
-		renderQueue.endAll();
-		return choice;
-	}
+    public void renderMessage(Message message,
+            TextToSpeechPlayer speechSynthesizer, ImageIterator dominantImages,
+            String attitude) {
+        completeAll();
+        renderQueue.start(deferredRenderers, teaseLib);
+        deferredRenderers.clear();
+        Set<String> hints = new HashSet<>();
+        // Within messages, images might change fast, and changing
+        // the camera position, image size or aspect would be too distracting
+        hints.add(ImageIterator.SameCameraPosition);
+        hints.add(ImageIterator.SameResolution);
+        hints.add(attitude);
+        RenderMessage renderMessage = new RenderMessage(message,
+                speechSynthesizer, dominantImages, hints);
+        renderQueue.start(renderMessage, teaseLib);
+    }
+
+    public String showChoices(Runnable scriptFunction,
+            final List<String> choices) {
+        // arguments check
+        for (String choice : choices) {
+            if (choice == null) {
+                throw new IllegalArgumentException("Choice may not be null");
+            }
+        }
+        TeaseLib.log("choose: " + choices.toString());
+        // Speech recognition
+        List<Integer> srChoiceIndices = new ArrayList<>(1);
+        Event<SpeechRecognitionImplementation, SpeechRecognizedEventArgs> speechRecognizedEvent = new Event<SpeechRecognitionImplementation, SpeechRecognizedEventArgs>() {
+            @Override
+            public void run(SpeechRecognitionImplementation sender,
+                    SpeechRecognizedEventArgs eventArgs) {
+                if (eventArgs.result.length == 1) {
+                    List<Delegate> uiElements = teaseLib.host
+                            .getClickableChoices(choices);
+                    // Click the button
+                    SpeechRecognitionResult speechRecognitionResult = eventArgs.result[0];
+                    if (speechRecognitionResult.isChoice(choices)) {
+                        // This assigns the result even if the buttons have
+                        // unrealized
+                        int choice = speechRecognitionResult.index;
+                        srChoiceIndices.add(choice);
+                        try {
+                            Delegate delegate = uiElements.get(choice);
+                            if (delegate != null) {
+                                delegate.run();
+                            } else {
+                                TeaseLib.log("Button gone for choice " + choice
+                                        + ": " + speechRecognitionResult.text);
+                            }
+                        } catch (Throwable t) {
+                            TeaseLib.log(this, t);
+                        }
+                    }
+                } else {
+                    // TODO none or more than one result means incorrect
+                    // recognition
+                }
+            }
+        };
+        speechRecognizer.events.recognitionCompleted.add(speechRecognizedEvent);
+        speechRecognizer.startRecognition(choices);
+        // Script closure
+        FutureTask<String> scriptTask = scriptFunction == null ? null
+                : new FutureTask<>(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        try {
+                            scriptFunction.run();
+                        } catch (ScriptInterruptedException e) {
+                            return null;
+                        }
+                        // Script function finished, click any and return
+                        // timeout
+                        List<Delegate> clickables = teaseLib.host
+                                .getClickableChoices(choices);
+                        if (!clickables.isEmpty()) {
+                            Delegate clickable = clickables.get(0);
+                            if (clickable != null) {
+                                clickables.get(0).run();
+                            } else {
+                                // Host implementation is incomplete
+                                new IllegalStateException(
+                                        "Host didn't return clickables for choices: "
+                                                + choices.toString());
+                            }
+                        }
+                        return TeaseScript.Timeout;
+                    }
+                });
+        // Get the user's choice
+        int choiceIndex;
+        try {
+            if (scriptTask != null) {
+                ExecutorService executor = Executors.newFixedThreadPool(1);
+                executor.execute(scriptTask);
+                renderQueue.completeStarts();
+                // TODO completeStarts() doesn't work because first we need to
+                // wait for render threads that can be completed
+                // Workaround: A bit unsatisfying, but otherwise the choice
+                // buttons would appear too early
+                teaseLib.host.sleep(300);
+            }
+            choiceIndex = teaseLib.host.choose(choices);
+            if (scriptTask != null) {
+                scriptTask.cancel(true);
+            }
+            speechRecognizer.completeSpeechRecognitionInProgress();
+        } finally {
+            speechRecognizer.stopRecognition();
+            speechRecognizer.events.recognitionCompleted
+                    .remove(speechRecognizedEvent);
+        }
+        // Assign result from speech recognition, tasks or button click
+        String choice = null;
+        if (!srChoiceIndices.isEmpty()) {
+            // Use the first speech recognition result
+            choiceIndex = srChoiceIndices.get(0);
+            choice = choices.get(choiceIndex);
+        } else {
+            String r = null;
+            // Script function completed or cancelled?
+            if (scriptTask != null) {
+                if (!scriptTask.isCancelled()) {
+                    try {
+                        // Wait for script task completion
+                        r = scriptTask.get();
+                        if (r == Timeout) {
+                            // Task completed
+                            choice = Timeout;
+                        }
+                    } catch (InterruptedException e) {
+                        throw new ScriptInterruptedException();
+                    } catch (ExecutionException e) {
+                        Throwable cause = e.getCause();
+                        if (cause != null) {
+                            // Forward error from closure to main thread
+                            throw new RuntimeException(cause);
+                        } else {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
+        renderQueue.endAll();
+        if (choice == null) {
+            choice = choices.get(choiceIndex);
+        }
+        return choice;
+    }
 }

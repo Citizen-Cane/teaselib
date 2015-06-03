a14fa7648d81baa57f07161399a2635486234f1d
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2015-06-03 12:05:58 +0200

    Fixed: a wait duration of 0 is a proper delay argument, but may hang a host that's just calling wait(...)

--- src/teaselib/text/RenderDelay.java
+++ src/teaselib/text/RenderDelay.java
@@ -5,36 +5,40 @@ import teaselib.TeaseLib;
 import teaselib.userinterface.MediaRendererThread;
 
 public class RenderDelay extends MediaRendererThread {
-	public final int from;
-	public final int to;
+    public final int from;
+    public final int to;
 
-	public RenderDelay(int delay) {
-		from = to = delay;
-	}
+    public RenderDelay(int delay) {
+        from = to = delay;
+    }
 
-	public RenderDelay(int from, int to) {
-		this.from = from;
-		this.to = to;
-	}
+    public RenderDelay(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
 
-	@Override
-	public void render() throws InterruptedException {
-		int actual = teaseLib.host.getRandom(from, to);
-		TeaseLib.log(getClass().getSimpleName() + " " + toString() + ": " + actual + " seconds");
-		startCompleted();
-		try {
-			teaseLib.host.sleep(actual * 1000);
-		} catch (ScriptInterruptedException e) {
-			// Expected
-		}
-		finally
-		{
-			mandatoryCompleted();
-		}
-	}
+    @Override
+    public void render() throws InterruptedException {
+        int actual = teaseLib.host.getRandom(from, to);
+        startCompleted();
+        try {
+            if (actual > 0) {
+                TeaseLib.log(getClass().getSimpleName() + " " + toString()
+                        + ": " + actual + " seconds");
+                teaseLib.host.sleep(actual * 1000);
+            } else {
+                TeaseLib.log(getClass().getSimpleName() + " " + toString()
+                        + ": skipped sleeping " + actual + " seconds");
+            }
+        } catch (ScriptInterruptedException e) {
+            // Expected
+        } finally {
+            mandatoryCompleted();
+        }
+    }
 
-	@Override
-	public String toString() {
-		return from + "-"+ to;
-	}
+    @Override
+    public String toString() {
+        return from + "-" + to;
+    }
 }

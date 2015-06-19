1ca49c8624171227ab61d5409ce6758953123348
Citizen-Cane <Citizen-Cane@github.com>
2015-06-19 08:05:46 +0200

    Constructor has to be public

--- src/teaselib/stimulation/StimulationController.java
+++ src/teaselib/stimulation/StimulationController.java
@@ -23,7 +23,7 @@ public class StimulationController<StimulationType> {
 
     private int intensity;
 
-    StimulationController() {
+    public StimulationController() {
         clear();
     }
 

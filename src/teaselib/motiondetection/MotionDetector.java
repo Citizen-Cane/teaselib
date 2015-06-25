e39282dbdad3d4f07669167141b9c9e6f1cf70d1
Citizen-Cane <Citizen-Cane@github.com>
2015-06-25 23:49:17 +0200

    Fixed await*(...) to return immediately when motion has already started/ended when the function is called; fixed thread to just signal motion start and stop once instead of signalling motion start all the time; cleaned up the code some more

--- src/teaselib/motiondetection/MotionDetector.java
+++ src/teaselib/motiondetection/MotionDetector.java
@@ -28,7 +28,7 @@ public class MotionDetector {
     }
 
     public enum Amount {
-        JustABit(0.1), SomeWhat(0.25), ALot(0.5);
+        None(0), JustABit(0.1), SomeWhat(0.25), ALot(0.5);
 
         public final double ScreenPercentage;
 
@@ -76,7 +76,7 @@ public class MotionDetector {
     }
 
     public void init(Webcam webcam) {
-        print(webcam.getName());
+        TeaseLib.log(webcam.getName());
         show(webcam);
         t = new WebCamThread();
         t.start();
@@ -112,25 +112,23 @@ public class MotionDetector {
 
         @Override
         public void run() {
-            // This value should be high?
-            detector.setInterval(PollingInterval); // one check per 100 ms
             // Longer values join short motions into a single motion, but
             // also increase response time
-            // detector.setInertia(500); // keep "motion" state for 100ms
-            // seconds
-            // Manage inertia ourselves, to avoid wrong isMotion reports
+            detector.setInterval(PollingInterval); // one check per 100 ms
+            // Manage inertia manually, to avoid wrong isMotion reports
             // with the image center as the image c.o.g.
             detector.clearInertia();
-            // How much of the image must move in order to recognize image
+            // Percentage of the image that has to change in order to recognize
             // changes it as motion
             detector.setAreaThreshold(AreaThresholdPercent);
             // Pixels are considered to be changed when they're above the
             // pixel threshold
             detector.setPixelThreshold(PixelThreshold);
             detector.start();
-            int motion = -1;
+            int motionDetectedCounter = -1;
             while (true) {
-                if (detector.isMotion()) {
+                boolean motionDetected = detector.isMotion();
+                if (motionDetected) {
                     motionCog = detector.getMotionCog();
                     xi.add(motionCog.x);
                     yi.add(motionCog.y);
@@ -149,24 +147,23 @@ public class MotionDetector {
                             frames, threshold));
                 }
                 // After setting current state, send any notifications
-                if (detector.isMotion()) {
-                    if (motion < 0) {
-                        printDebug();
+                if (motionDetected) {
+                    if (motionDetectedCounter < 0) {
+                        // Motion just started
                         signalMotionStart();
+                        printDebug();
                     } else {
                         printDebug();
-                        signalMotionStart();
                     }
-                    motion = MotionInertia;
+                    motionDetectedCounter = MotionInertia;
                 } else {
-                    if (motion == 0) {
+                    if (motionDetectedCounter == 0) {
                         printDebug();
                         signalMotionEnd();
-                        motion = -1;
-                    } else if (motion > 0) {
+                        motionDetectedCounter = -1;
+                    } else if (motionDetectedCounter > 0) {
                         // inertia
-                        motion--;
-                        signalMotionStart();
+                        motionDetectedCounter--;
                     }
                 }
 
@@ -193,6 +190,7 @@ public class MotionDetector {
         public void signalMotionStart() {
             motionStartLock.lock();
             try {
+                TeaseLib.log("Motion started");
                 motionStart.signalAll();
             } finally {
                 motionStartLock.unlock();
@@ -202,15 +200,12 @@ public class MotionDetector {
         public void signalMotionEnd() {
             motionEndLock.lock();
             try {
+                TeaseLib.log("Motion ended");
                 motionEnd.signalAll();
             } finally {
                 motionEndLock.unlock();
             }
         }
-    }
-
-    private static void print(String text) {
-        TeaseLib.log(text);
     }
 
     private static HorizontalMotion getHorizontalMotion(Direction dx) {
@@ -260,60 +255,81 @@ public class MotionDetector {
             double pastSeconds) {
         HorizontalMotion horizontalMotion = getHorizontalMotion(xi
                 .getDirection(frames(pastSeconds), MotionThreshold));
-        print("getRecentHorizontalMotion(" + pastSeconds + ") returned "
+        TeaseLib.log("getRecentHorizontalMotion(" + pastSeconds + ") returned "
                 + horizontalMotion.toString());
         return horizontalMotion;
     }
 
     public Amount getAmountOfMotion(double pastSeconds) {
-        int dx = Math.abs(xi.distance(frames(pastSeconds)));
-        int dy = Math.abs(yi.distance(frames(pastSeconds)));
+        return getAmountOfMotion(frames(pastSeconds));
+    }
+
+    public Amount getAmountOfMotion(int pastFrames) {
+        int dx = Math.abs(xi.distance(pastFrames));
+        int dy = Math.abs(yi.distance(pastFrames));
         Amount[] values = Amount.values();
         for (Amount amount : values) {
-            if (dx < amount.ScreenPercentage * size.width
-                    && dy < amount.ScreenPercentage * size.height) {
+            if (dx <= amount.ScreenPercentage * size.width
+                    && dy <= amount.ScreenPercentage * size.height) {
                 return amount;
             }
         }
-        return Amount.ALot;
+        return Amount.None;
     }
 
     public synchronized VerticalMotion getRecentVerticalMotion(
             double pastSeconds) {
         VerticalMotion verticalMotion = getVerticalMotion(yi.getDirection(
                 frames(pastSeconds), MotionThreshold));
-        print("getRecentVerticalMotion(" + pastSeconds + ") returned "
+        TeaseLib.log("getRecentVerticalMotion(" + pastSeconds + ") returned "
                 + verticalMotion.toString());
         return verticalMotion;
     }
 
     // todo set length of motion
+    /**
+     * Waits the specified period for motion.
+     * 
+     * @param timeoutSeconds
+     * @return True if motion started within the time period. False if no motion
+     *         was detected during the time period.
+     */
     public boolean awaitMotionStart(double timeoutSeconds) {
-        boolean motionDetected = false;
         motionStartLock.lock();
         try {
-            motionDetected = motionStart.await((long) timeoutSeconds * 1000,
-                    TimeUnit.MILLISECONDS);
+            boolean motionDetected = getAmountOfMotion(MotionInertia) != MotionDetector.Amount.None;
+            if (!motionDetected) {
+                motionDetected = motionStart.await(
+                        (long) timeoutSeconds * 1000, TimeUnit.MILLISECONDS);
+            }
+            return motionDetected;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
             motionStartLock.unlock();
         }
-        return motionDetected;
     }
 
+    /**
+     * @param timeoutSeconds
+     * @return True if motion stopped within the time period. False if motion is
+     *         still detected at the end of the time period.
+     */
+    // todo must return immediately when there isn't any motion
     public boolean awaitMotionEnd(double timeoutSeconds) {
-        boolean motionStopped = false;
         motionEndLock.lock();
         try {
-            motionStopped = motionEnd.await((long) timeoutSeconds * 1000,
-                    TimeUnit.MILLISECONDS);
+            boolean motionStopped = getAmountOfMotion(MotionInertia) == MotionDetector.Amount.None;
+            if (!motionStopped) {
+                motionStopped = motionEnd.await((long) timeoutSeconds * 1000,
+                        TimeUnit.MILLISECONDS);
+            }
+            return motionStopped;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
             motionEndLock.unlock();
         }
-        return motionStopped;
     }
 
     /**

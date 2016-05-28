79785d9ab37eccee47084e662ad71a6118ebf7bb
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2016-05-28 11:12:08 +0200

    user code methods for persence

---
+++ src/teaselib/motiondetection/Presence.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package teaselib.motiondetection;
+
+/**
+ * @author someone
+ *
+ */
+public class Presence {
+    private final MotionDetector motionDetector;
+
+    Presence(MotionDetector motionDetector) {
+        this.motionDetector = motionDetector;
+    }
+
+    public boolean isPresentSince(double timeSpanSeconds) {
+        return motionDetector.awaitChange(1.0,
+                teaselib.motiondetection.MotionDetector.Presence.Present,
+                timeSpanSeconds, 0.0);
+    }
+
+    public boolean isAwaySince(double timeSpanSeconds) {
+        return motionDetector.awaitChange(1.0,
+                teaselib.motiondetection.MotionDetector.Presence.Present,
+                timeSpanSeconds, 0.0);
+    }
+
+    public boolean awaitPresence(double presenceTimeSpanSeconds,
+            double timeoutSeconds) {
+        return motionDetector.awaitChange(0.8,
+                teaselib.motiondetection.MotionDetector.Presence.Present,
+                presenceTimeSpanSeconds, timeoutSeconds);
+    }
+
+    public boolean awaitAway(double presenceTimeSpanSeconds,
+            double timeoutSeconds) {
+        return motionDetector.awaitChange(0.8,
+                teaselib.motiondetection.MotionDetector.Presence.Away,
+                presenceTimeSpanSeconds, timeoutSeconds);
+    }
+}

1a59c041ca26d62195072c60a0a31f0ec5cc24d6
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2016-06-12 19:05:38 +0200

    Adjusted the timing, changed hard-coded values to constants

--- src.test.interactive/teaselib/stimulation/XInputOutputTestAlternate.java
+++ src.test.interactive/teaselib/stimulation/XInputOutputTestAlternate.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package teaselib.stimulation;
 
 import org.junit.BeforeClass;
@@ -12,47 +9,46 @@ import teaselib.core.devices.xinput.XInputComponents;
 import teaselib.core.devices.xinput.XInputDevice;
 import teaselib.core.devices.xinput.XInputDevices;
 
-/**
- * @author someone
- *
- */
 public class XInputOutputTestAlternate {
 
-    /**
-     * @throws java.lang.Exception
-     */
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
+    public static void setUpBeforeClass() {
     }
 
     @Test
-    public void testTimeoutHack_1h() throws InterruptedException {
+    public void testAlternate() throws InterruptedException {
         XInputDevice xid = XInputDevices.Instance.getDefaultDevice();
         System.out.println(xid.getDevicePath());
         for (int i = 0; i < 1000; i++) {
             try {
-                testVibration(xid, (short) 65535, (short) 0);
-                testVibration(xid, (short) 0, (short) 0);
-                testVibration(xid, (short) 0, (short) 65535);
+                testVibration(xid, XInputDevice.VIBRATION_MAX_VALUE,
+                        XInputDevice.VIBRATION_MIN_VALUE);
+                testVibration(xid, XInputDevice.VIBRATION_MIN_VALUE,
+                        XInputDevice.VIBRATION_MAX_VALUE);
+                testVibration(xid, XInputDevice.VIBRATION_MIN_VALUE,
+                        XInputDevice.VIBRATION_MIN_VALUE);
+                testVibration(xid, XInputDevice.VIBRATION_MAX_VALUE,
+                        XInputDevice.VIBRATION_MAX_VALUE);
             } finally {
-                testVibration(xid, (short) 0, (short) 0);
+                testVibration(xid, XInputDevice.VIBRATION_MIN_VALUE,
+                        XInputDevice.VIBRATION_MIN_VALUE);
             }
         }
     }
 
-    private static void testVibration(XInputDevice xid, short left, short right)
+    private static void testVibration(XInputDevice xid, int left, int right)
             throws InterruptedException {
         XInputComponents xic;
         XInputButtons xib;
         XInputAxes xia;
         xid.setVibration(left, right);
-        Thread.sleep(500);
+        Thread.sleep(2500);
         xid.poll();
         xic = xid.getComponents();
         xib = xic.getButtons();
         xia = xic.getAxes();
-        System.out.println(
-                xia.lt + ", " + xia.rt + ", " + xib.lThumb + ", " + xib.rThumb);
-        Thread.sleep(3500);
+        System.out.println(xia.lt + ", " + xia.rt + ", " + xib.lShoulder + ", "
+                + xib.rShoulder);
+        Thread.sleep(2500);
     }
 }

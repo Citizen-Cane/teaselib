06bba01ee7334b38401abae49604f3ee83e8068a
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2016-12-07 21:40:36 +0100

    Added constant punish waveform, using it as default

--- src/teaselib/stimulation/pattern/Punish.java
+++ src/teaselib/stimulation/pattern/Punish.java
@@ -4,6 +4,7 @@
 package teaselib.stimulation.pattern;
 
 import teaselib.stimulation.BurstSquareWave;
+import teaselib.stimulation.SquareWave;
 import teaselib.stimulation.Stimulation;
 import teaselib.stimulation.Stimulator;
 import teaselib.stimulation.WaveForm;
@@ -14,16 +15,31 @@ import teaselib.stimulation.WaveForm;
  */
 public class Punish extends Stimulation {
 
+    private final PunishType punishType;
+
+    enum PunishType {
+        Constant,
+        Burst
+    }
+
     public Punish(Stimulator stimulator) {
+        this(stimulator, PunishType.Constant);
+    }
+
+    public Punish(Stimulator stimulator, PunishType punishType) {
         super(stimulator, 0.0);
+        this.punishType = punishType;
     }
 
     @Override
     public WaveForm waveform(int intensity) {
         double punishSeconds = Punish.getSeconds(intensity);
-        // A constant signal
-        return new BurstSquareWave(punishSeconds * 1000, punishSeconds * 1000,
-                0.05 * 1000);
+        if (punishType == PunishType.Constant) {
+            return new SquareWave(punishSeconds * 1000, punishSeconds * 1000);
+        } else {
+            return new BurstSquareWave(punishSeconds * 1000,
+                    punishSeconds * 1000, 0.05 * 1000);
+        }
     }
 
     public static double getSeconds(int intensity) {

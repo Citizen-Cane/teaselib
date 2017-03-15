f0e95301876b5520dc8680f2992188228ebe62cf
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-03-15 16:23:54 +0100

    New state concept - WIP
    Paired states, multiple reason for applied states; duration

---
+++ src.test/teaselib/core/EnumStateMapsTest.java
@@ -0,0 +1,204 @@
+package teaselib.core;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static teaselib.Toys.Clothespins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import teaselib.Body;
+import teaselib.Toys;
+import teaselib.test.TestScript;
+
+public class EnumStateMapsTest extends EnumStateMaps {
+
+    public EnumStateMapsTest() {
+        super(TestScript.getOne().teaseLib);
+    }
+
+    @Test
+    public void testCannotJerkOff() throws Exception {
+        State chastityCage = state(Toys.Chastity_Cage);
+        State wristRestraints = state(Toys.Leather_Wrist_Cuffs);
+
+        assertFalse(chastityCage.applied());
+        assertFalse(wristRestraints.applied());
+
+        State somethingOnPenis = state(Body.SomethingOnPenis);
+        State cannotJerkOff = state(Body.CannotJerkOff);
+        State handsTiedBehindBack = state(Body.WristsTiedBehindBack);
+
+        chastityCage.apply(Body.SomethingOnPenis, Body.CannotJerkOff);
+        wristRestraints.apply(Body.WristsTiedBehindBack, Body.CannotJerkOff);
+
+        assertTrue(chastityCage.applied());
+        assertTrue(somethingOnPenis.applied());
+        assertTrue(cannotJerkOff.applied());
+
+        assertTrue(wristRestraints.applied());
+        assertTrue(handsTiedBehindBack.applied());
+
+        chastityCage.remove();
+
+        assertFalse(chastityCage.applied());
+        assertFalse(somethingOnPenis.applied());
+        assertTrue(cannotJerkOff.applied());
+
+        assertTrue(wristRestraints.applied());
+        assertTrue(handsTiedBehindBack.applied());
+
+        wristRestraints.remove();
+
+        assertFalse(wristRestraints.applied());
+        assertFalse(handsTiedBehindBack.applied());
+    }
+
+    @Test
+    public void testPegsOnNipplesAndNutsApplyOneAfterAnother()
+            throws Exception {
+        State clothesPins = state(Toys.Clothespins);
+
+        State somethingOnNipples = state(Body.SomethingOnNipples);
+        State somethingOnBalls = state(Body.SomethingOnBalls);
+
+        state(Body.SomethingOnNipples).apply(Toys.Clothespins);
+
+        assertTrue(clothesPins.applied());
+        assertTrue(somethingOnNipples.applied());
+        assertFalse(somethingOnBalls.applied());
+
+        state(Body.SomethingOnBalls).apply(Toys.Clothespins);
+
+        assertTrue(clothesPins.applied());
+        assertTrue(somethingOnNipples.applied());
+        assertTrue(somethingOnBalls.applied());
+
+        clothesPins.remove();
+
+        assertFalse(clothesPins.applied());
+        assertFalse(somethingOnNipples.applied());
+        assertFalse(somethingOnBalls.applied());
+    }
+
+    @Test
+    public void testPegsOnNipplesAndNutsRemoveOneAfterAnother()
+            throws Exception {
+        State clothesPins = state(Toys.Clothespins);
+
+        State somethingOnNipples = state(Body.SomethingOnNipples);
+        State somethingOnBalls = state(Body.SomethingOnBalls);
+
+        state(Clothespins).apply(Body.SomethingOnBalls,
+                Body.SomethingOnNipples);
+
+        assertTrue(clothesPins.applied());
+        assertTrue(somethingOnNipples.applied());
+        assertTrue(somethingOnBalls.applied());
+
+        somethingOnBalls.remove(Toys.Clothespins);
+
+        assertTrue(clothesPins.applied());
+        assertTrue(somethingOnNipples.applied());
+        assertFalse(somethingOnBalls.applied());
+
+        somethingOnNipples.remove(Toys.Clothespins);
+
+        assertFalse(clothesPins.applied());
+        assertFalse(somethingOnNipples.applied());
+        assertFalse(somethingOnBalls.applied());
+    }
+
+    @Test
+    public void testLockUp() throws Exception {
+        State chastityCage = state(Toys.Chastity_Cage);
+        State wristRestraints = state(Toys.Leather_Wrist_Cuffs);
+        State key = state(Toys.Chastity_Device_Lock);
+
+        assertFalse(chastityCage.applied());
+        assertFalse(wristRestraints.applied());
+        assertFalse(key.applied());
+
+        State somethingOnPenis = state(Body.SomethingOnPenis);
+        State cannotJerkOff = state(Body.CannotJerkOff);
+        State handsTiedBehindBack = state(Body.WristsTiedBehindBack);
+
+        chastityCage.apply(Body.SomethingOnPenis, Body.CannotJerkOff);
+        chastityCage.apply(Toys.Chastity_Device_Lock);
+        wristRestraints.apply(Body.WristsTiedBehindBack, Body.CannotJerkOff);
+
+        assertTrue(chastityCage.applied());
+        assertTrue(key.applied());
+        assertTrue(somethingOnPenis.applied());
+        assertTrue(cannotJerkOff.applied());
+
+        assertTrue(wristRestraints.applied());
+        assertTrue(handsTiedBehindBack.applied());
+
+        key.remove();
+
+        assertFalse(key.applied());
+
+        assertTrue(chastityCage.applied());
+        assertTrue(somethingOnPenis.applied());
+        assertTrue(cannotJerkOff.applied());
+
+        assertTrue(wristRestraints.applied());
+        assertTrue(handsTiedBehindBack.applied());
+
+        chastityCage.remove();
+
+        assertFalse(chastityCage.applied());
+        assertFalse(somethingOnPenis.applied());
+        assertTrue(cannotJerkOff.applied());
+
+        assertTrue(wristRestraints.applied());
+        assertTrue(handsTiedBehindBack.applied());
+
+        wristRestraints.remove();
+
+        assertFalse(wristRestraints.applied());
+        assertFalse(handsTiedBehindBack.applied());
+    }
+
+    @Test
+    public void testLockDuration() throws Exception {
+        State chastityCage = state(Toys.Chastity_Cage);
+        State key = state(Toys.Chastity_Device_Lock);
+
+        assertFalse(chastityCage.applied());
+        assertFalse(key.applied());
+
+        chastityCage.apply(Body.SomethingOnPenis, Body.CannotJerkOff);
+        assertTrue(state(Body.SomethingOnPenis).applied());
+        assertTrue(state(Body.CannotJerkOff).applied());
+
+        key.apply(Toys.Chastity_Cage);
+
+        assertTrue(chastityCage.applied());
+        assertTrue(chastityCage.expired());
+        assertTrue(key.applied());
+        assertTrue(key.expired());
+
+        key.apply(Toys.Chastity_Cage).upTo(24, TimeUnit.HOURS);
+        assertTrue(key.applied());
+        assertTrue(chastityCage.applied());
+
+        assertFalse(key.expired());
+        assertFalse(chastityCage.expired());
+
+        key.remove();
+        assertFalse(key.applied());
+        assertTrue(key.expired());
+        assertTrue(chastityCage.applied());
+        assertFalse(chastityCage.expired());
+
+        chastityCage.remove();
+        assertFalse(chastityCage.applied());
+        assertTrue(chastityCage.expired());
+
+        assertFalse(state(Body.SomethingOnPenis).applied());
+        assertFalse(state(Body.SomethingOnPenis).applied());
+    }
+}
---
+++ src/teaselib/core/EnumStateMaps.java
@@ -0,0 +1,207 @@
+package teaselib.core;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import teaselib.core.TeaseLib.PersistentString;
+
+public class EnumStateMaps {
+
+    private final Map<Class<?>, EnumStateMap<? extends Enum<?>>> stateMaps = new HashMap<Class<?>, EnumStateMap<? extends Enum<?>>>();
+    private final TeaseLib teaseLib;
+
+    public EnumStateMaps(TeaseLib teaseLib) {
+        this.teaseLib = teaseLib;
+    }
+
+    interface State {
+        public static final int REMOVEED = -1;
+
+        State.Options apply();
+
+        <S extends Enum<?>> State.Options apply(S... reason);
+
+        boolean applied();
+
+        boolean expired();
+        // TeaseLib.Duration duration();
+
+        <S extends Enum<?>> State remove();
+
+        <S extends Enum<?>> State remove(S reason);
+
+        interface Options extends State.Persistence {
+            Persistence upTo(long duration, TimeUnit unit);
+        }
+
+        interface Persistence {
+            State remember();
+        }
+    }
+
+    public class EnumStateMap<T extends Enum<?>> {
+        final Map<T, State> states = new HashMap<T, State>();
+
+        public State get(T item) {
+            if (states.containsKey(item)) {
+                return states.get(item);
+            } else {
+                State state = new EnumState<T>(item);
+                states.put(item, state);
+                return state;
+            }
+        }
+    }
+
+    public class EnumState<T extends Enum<?>> implements State, State.Options {
+        private final T item;
+        private final Set<Enum<?>> reasons = new HashSet<Enum<?>>();
+        private final PersistentString storage;
+
+        private TeaseLib.Duration duration = teaseLib.new Duration(REMOVEED,
+                TimeUnit.SECONDS);
+
+        public EnumState(T item) {
+            super();
+            this.item = item;
+            this.storage = teaseLib.new PersistentString(TeaseLib.DefaultDomain,
+                    namespaceOf(item), nameOf(item));
+        }
+
+        private String namespaceOf(T item) {
+            return item.getClass().getName();
+        }
+
+        private String nameOf(T item) {
+            return item.name() + ".state";
+        }
+
+        @Override
+        public State.Options apply() {
+            remove();
+            return this;
+        }
+
+        // protected <S extends Enum<?>> State.Options apply(S reason) {
+        // if (!reasons.contains(reason)) {
+        // reasons.add(reason);
+        // state(reason).apply(item);
+        // }
+        // return this;
+        // }
+
+        @Override
+        public <S extends Enum<?>> State.Options apply(S... reason) {
+            for (S s : reason) {
+                if (!reasons.contains(s)) {
+                    reasons.add(s);
+                    Enum<?>[] items = new Enum<?>[] { item };
+                    state(s).apply(items);
+                }
+            }
+            return this;
+        }
+
+        @Override
+        public State.Persistence upTo(long limit, TimeUnit unit) {
+            this.duration = teaseLib.new Duration(limit, unit);
+            for (Enum<?> reason : reasons) {
+                EnumState<?> state = (EnumState<?>) state(reason);
+                state.setDuration(limit, unit);
+            }
+            return this;
+        }
+
+        State setDuration(long limit, TimeUnit unit) {
+            this.duration = teaseLib.new Duration(limit, unit);
+            return this;
+        }
+
+        @Override
+        public State remember() {
+            return this;
+        }
+
+        @Override
+        public boolean applied() {
+            return !reasons.isEmpty();
+        }
+
+        @Override
+        public boolean expired() {
+            return duration.expired();
+        }
+
+        @Override
+        public EnumState<T> remove() {
+            Enum<?>[] copyOfReasons = new Enum<?>[reasons.size()];
+            for (Enum<?> reason : reasons.toArray(copyOfReasons)) {
+                state(reason).remove(item);
+            }
+            reasons.clear();
+            setDuration(REMOVEED, TimeUnit.SECONDS);
+            return this;
+        }
+
+        @Override
+        public <S extends Enum<?>> State remove(S reason) {
+            if (reasons.contains(reason)) {
+                reasons.remove(reason);
+                state(reason).remove(item);
+            }
+            if (reasons.isEmpty()) {
+                setDuration(REMOVEED, TimeUnit.SECONDS);
+            }
+            return this;
+        }
+    }
+
+    /**
+     * Return the state of an enumeration member
+     * 
+     * @param item
+     *            The enumeration member to return the state for
+     * @return The item state.
+     */
+    @SuppressWarnings("unchecked")
+    public <T extends Enum<?>> State state(T item) {
+        return state((Class<T>) item.getClass()).get(item);
+    }
+
+    /**
+     * Return the state for all or a subset of members of an enumeration.
+     * 
+     * @param values
+     *            The values to retrieve the state for. This should be
+     *            {@code Enum.values()}, as the state will only contain the
+     *            state items for the listed values.
+     * @return The state of all members in {@code values}.
+     */
+    @SuppressWarnings("unchecked")
+    <T extends Enum<?>> EnumStateMap<T> state(T[] values) {
+        return state((Class<T>) values[0].getClass());
+    }
+
+    /**
+     * Return the state for all members of an enumeration.
+     * 
+     * @param enumClass
+     *            The class of the enumeration.
+     * @return The state of all members of the enumeration.
+     */
+    @SuppressWarnings("unchecked")
+    private <T extends Enum<?>> EnumStateMap<T> state(Class<T> enumClass) {
+        final EnumStateMap<T> stateMap;
+        if (stateMaps.containsKey(enumClass)) {
+            stateMap = (EnumStateMap<T>) stateMaps.get(enumClass);
+        } else {
+            stateMap = new EnumStateMap<T>();
+            stateMaps.put(enumClass, stateMap);
+        }
+        return stateMap;
+    }
+
+}

64d8c85f6443c9600dd5e14bc5933807554501f5
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-06-26 18:35:49 +0200

    Replaced the worker thread with an executor service

--- src/teaselib/core/ui/HostInputMethod.java
+++ src/teaselib/core/ui/HostInputMethod.java
@@ -1,10 +1,11 @@
 package teaselib.core.ui;
 
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.locks.ReentrantLock;
 
 import teaselib.core.Host;
-import teaselib.core.ScriptInterruptedException;
+import teaselib.core.concurrency.NamedExecutorService;
 import teaselib.core.ui.PromptQueue.Todo;
 import teaselib.core.ui.PromptQueue.Todo.Action;
 
@@ -15,61 +16,48 @@ import teaselib.core.ui.PromptQueue.Todo.Action;
 public class HostInputMethod implements InputMethod {
     private final Host host;
 
-    Thread workerThread;
+    ExecutorService workerThread = NamedExecutorService.singleThreadedQueue(getClass().getName());
+
     private final ReentrantLock replySection = new ReentrantLock();
-    private final SynchronousQueue<Todo> todos = new SynchronousQueue<Todo>();
 
     public HostInputMethod(Host host) {
         super();
         this.host = host;
-
-        workerThread = new Thread(worker);
-        workerThread.setName(getClass().getSimpleName() + "@" + hashCode());
-        workerThread.setDaemon(true);
-        workerThread.start();
     }
-
-    Runnable worker = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                while (!Thread.interrupted()) {
-                    Todo todo = todos.take();
-                    synchronized (HostInputMethod.this) {
-                        if (todo.action == Action.Show) {
-                            replySection.lockInterruptibly();
-                            try {
-                                final int reply = host.reply(todo.prompt.derived);
-                                if (todo.result == PromptQueue.UNDEFINED) {
-                                    todo.result = reply;
-                                }
-                            } finally {
-                                notifyWaiters();
-                                synchronized (todo.prompt) {
-                                    todo.prompt.notifyAll();
-                                }
-                                replySection.unlock();
-                            }
-                        }
-                    }
-                }
-            } catch (InterruptedException e) {
-                // Expected
-            }
-        }
-    };
 
     private void notifyWaiters() {
         notifyAll();
     }
 
     @Override
-    public void show(Todo todo) {
-        try {
-            todos.put(todo);
-        } catch (InterruptedException e) {
-            throw new ScriptInterruptedException();
-        }
+    public void show(final Todo todo) {
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                synchronized (HostInputMethod.this) {
+                    if (todo.action == Action.Show) {
+                        replySection.lockInterruptibly();
+                        try {
+                            final int reply = host.reply(todo.prompt.derived);
+                            if (todo.result == PromptQueue.UNDEFINED) {
+                                todo.result = reply;
+                            }
+                        } finally {
+                            notifyWaiters();
+                            synchronized (todo.prompt) {
+                                todo.prompt.notifyAll();
+                            }
+                            replySection.unlock();
+                        }
+                    }
+
+                }
+                return todo.result;
+            }
+        };
+
+        workerThread.submit(callable);
+
     }
 
     @Override

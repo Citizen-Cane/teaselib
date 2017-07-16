e858e6c375302b3a518077bc726a5f50e36fc781
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-07-16 10:26:51 +0200

    Prototype for global object management

---
+++ src.test/teaselib/core/TeaseLibGlobalObjectsTest.java
@@ -0,0 +1,49 @@
+package teaselib.core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class TeaseLibGlobalObjectsTest {
+
+    private final Map<Object, Object> globals = new HashMap<Object, Object>();
+
+    enum TestObject {
+        Object1,
+        Object2
+    }
+
+    final TestObject key1 = TestObject.Object1;
+    final String value1 = new String("Object1");
+
+    final TestObject key2 = TestObject.Object2;
+    final Collection<?> value2 = Collections.EMPTY_LIST;
+
+    public <T> void storeGlobal(Object key, T value) {
+        globals.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getGlobal(Object key) {
+        return (T) globals.get(key);
+    }
+
+    @Test
+    public void testGlobalObjects() {
+        storeGlobal(TestObject.Object1, value1);
+        storeGlobal(TestObject.Object2, value2);
+
+        String retrieved1 = getGlobal(TestObject.Object1);
+        assertEquals(value1, retrieved1);
+
+        Collection<?> retrieved2 = getGlobal(TestObject.Object2);
+        assertEquals(0, retrieved2.size());
+        assertEquals(value2, retrieved2);
+    }
+
+}

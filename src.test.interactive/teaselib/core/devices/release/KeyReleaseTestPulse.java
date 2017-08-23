6a5e762df95af97d9e28fc7d7fa5e94136f07a31
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-08-23 18:28:54 +0200

    Added test to check key release pulse color and rate

---
+++ src.test.interactive/teaselib/core/devices/release/KeyReleaseTestPulse.java
@@ -0,0 +1,51 @@
+package teaselib.core.devices.release;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KeyReleaseTestPulse {
+    private static final Logger logger = LoggerFactory.getLogger(KeyReleaseTestPulse.class);
+
+    @Test
+    public void testPulseFrequency() {
+        final KeyRelease keyRelease = KeyReleaseTest.connectDefaultDevice();
+        List<Actuator> actuators = KeyReleaseTest.connect(keyRelease);
+
+        for (Actuator actuator : KeyReleaseTest.connect(keyRelease)) {
+            testActuator(actuator);
+        }
+
+        KeyReleaseTest.sleep(10, TimeUnit.SECONDS);
+        assertTrue(keyRelease.connected());
+        assertTrue(keyRelease.active());
+        assertTrue(actuators.size() > 0);
+    }
+
+    private static void testActuator(Actuator actuator) {
+        KeyReleaseTest.arm(actuator);
+        KeyReleaseTest.sleep(15, TimeUnit.SECONDS);
+
+        actuator.start(actuator.available(TimeUnit.SECONDS), TimeUnit.SECONDS);
+
+        while (actuator.isRunning()) {
+            long remaining = actuator.remaining(TimeUnit.SECONDS);
+            if (remaining == 0) {
+                break;
+            }
+            logger.info(
+                    "Actuator " + actuator + " has " + actuator.remaining(TimeUnit.MINUTES) + " minutes until release");
+            KeyReleaseTest.sleep(60, TimeUnit.SECONDS);
+        }
+
+        // Don't release, this should happen automatically
+
+        assertFalse(actuator.isRunning());
+        logger.info("Actuator " + actuator + " auto-released");
+    }
+}

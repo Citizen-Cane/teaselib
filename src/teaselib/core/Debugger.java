0e93d0c99c03a7d1196f34b34e9a1cb5875f64c1
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-09-14 20:38:30 +0200

    Added support for response building blocks
    - Introduced response action class
    - add collections of response actions
    -> used to modularize Mine automated script tests

--- src/teaselib/core/Debugger.java
+++ src/teaselib/core/Debugger.java
@@ -1,6 +1,7 @@
 package teaselib.core;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +12,17 @@ import teaselib.hosts.DummyPersistence;
 
 public class Debugger {
     public final TeaseLib teaseLib;
+
+    public static class ResponseAction {
+        final String match;
+        final Response response;
+
+        public ResponseAction(String match, Response response) {
+            super();
+            this.match = match;
+            this.response = response;
+        }
+    }
 
     public enum Response {
         Choose,
@@ -53,7 +65,17 @@ public class Debugger {
         responses.put(match, response);
     }
 
+    public void addResponse(ResponseAction responseAction) {
+        responses.put(responseAction.match, responseAction.response);
+    }
+
     public void clearStateMaps() {
         teaseLib.stateMaps.clear();
+    }
+
+    public void addResponses(Collection<ResponseAction> responses) {
+        for (ResponseAction responseAction : responses) {
+            addResponse(responseAction);
+        }
     }
 }

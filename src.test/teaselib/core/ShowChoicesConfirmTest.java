26633312d9f528c5b011f4ae227431172c06597f
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-12-18 22:23:33 +0100

    Added tests for timed prompts (confirm/auto-confirm)

---
+++ src.test/teaselib/core/ShowChoicesConfirmTest.java
@@ -0,0 +1,38 @@
+package teaselib.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import teaselib.ScriptFunction;
+import teaselib.core.speechrecognition.SpeechRecognition.TimeoutBehavior;
+import teaselib.test.TestScript;
+
+/**
+ * @author Citizen-Cane
+ *
+ */
+public class ShowChoicesConfirmTest {
+    @Test
+    public void testDismissScriptFunction() {
+        TestScript script = TestScript.getOne();
+        String choice = "Dismiss";
+        script.debugger.addResponse(choice, Debugger.Response.Choose);
+
+        script.say("Foobar");
+        assertEquals(choice,
+                script.reply(script.timeoutWithConfirmation(1, TimeoutBehavior.InDubioContraReum), choice));
+    }
+
+    @Test
+    public void testAwaitScriptFunctionTimeout() {
+        TestScript script = TestScript.getOne();
+        String choice = "Dismiss";
+
+        script.debugger.addResponse(choice, Debugger.Response.Ignore);
+
+        script.say("Foobar");
+        assertEquals(ScriptFunction.Timeout,
+                script.reply(script.timeoutWithConfirmation(1, TimeoutBehavior.InDubioContraReum), choice));
+    }
+}

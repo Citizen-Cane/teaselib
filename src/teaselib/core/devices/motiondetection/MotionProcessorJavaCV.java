87274ae1b3f067c06fee88d25fc2149def192b88
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-01-05 18:09:42 +0100

    Refactored javacv motion detector to prepare head gesture recognition

--- src/teaselib/core/devices/motiondetection/MotionProcessorJavaCV.java
+++ src/teaselib/core/devices/motiondetection/MotionProcessorJavaCV.java
@@ -1,21 +1,19 @@
 package teaselib.core.devices.motiondetection;
 
-import static teaselib.core.javacv.Color.Green;
-
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_core.Size;
 
 import teaselib.core.javacv.BackgroundSubtraction;
+import teaselib.core.javacv.Color;
 import teaselib.core.javacv.Contours;
 import teaselib.core.javacv.DistanceTracker;
-import teaselib.core.javacv.TrackFeatures;
 
 public class MotionProcessorJavaCV {
-    public final static Rect None = new Rect(Integer.MAX_VALUE,
-            Integer.MAX_VALUE, -Integer.MAX_VALUE, -Integer.MAX_VALUE);
-    public final static Rect Previous = new Rect(-Integer.MAX_VALUE,
-            -Integer.MAX_VALUE, -Integer.MAX_VALUE, -Integer.MAX_VALUE);
+    public final static Rect None = new Rect(Integer.MAX_VALUE, Integer.MAX_VALUE, -Integer.MAX_VALUE,
+            -Integer.MAX_VALUE);
+    public final static Rect Previous = new Rect(-Integer.MAX_VALUE, -Integer.MAX_VALUE, -Integer.MAX_VALUE,
+            -Integer.MAX_VALUE);
 
     private final int captureWidth;
     private final int renderWidth;
@@ -25,8 +23,9 @@ public class MotionProcessorJavaCV {
 
     BackgroundSubtraction motion;
     Contours motionContours = new Contours();
-    TrackFeatures trackFeatures = new TrackFeatures();
-    DistanceTracker distanceTracker = new DistanceTracker(Green);
+    DistanceTracker distanceTracker = new DistanceTracker(Color.Green);
+
+    DistanceTracker gestureTracker = new DistanceTracker(Color.Red);
 
     /**
      * @param captureSize
@@ -39,29 +38,25 @@ public class MotionProcessorJavaCV {
     }
 
     /**
-     * Calculate the size of the structuring element based on the size of the
-     * captured image.
+     * Calculate the size of the structuring element based on the size of the captured image.
      * 
      * @param nominalSizeAtBaseResolutionWidth
      */
-    public void setStructuringElementSize(
-            int nominalSizeAtBaseResolutionWidth) {
+    public void setStructuringElementSize(int nominalSizeAtBaseResolutionWidth) {
         double baseResolutionWidth = 1920;
         double nominalWidthFactor = captureWidth / baseResolutionWidth;
-        double sizeFactor = ((double) renderWidth) / ((double) captureWidth)
-                * nominalWidthFactor;
+        double sizeFactor = ((double) renderWidth) / ((double) captureWidth) * nominalWidthFactor;
         // The structuring element size must beat least 2
-        structuringElementSize = Math.max(2,
-                (int) (nominalSizeAtBaseResolutionWidth * sizeFactor));
+        structuringElementSize = Math.max(2, (int) (nominalSizeAtBaseResolutionWidth * sizeFactor));
         motion.setStructuringElementSize(structuringElementSize);
         distanceThreshold2 = structuringElementSize * structuringElementSize;
     }
 
-    public void update(Mat input) {
-        motion.update(input);
+    public void update(Mat videoImage) {
+        motion.update(videoImage);
         motionContours.update(motion.output);
-        if (trackFeatures.haveFeatures()) {
-            trackFeatures.update(input);
+        if (distanceTracker.hasFeatures()) {
+            distanceTracker.updateTrackerData(videoImage);
         }
     }
 
--- src/teaselib/core/javacv/DistanceTracker.java
+++ src/teaselib/core/javacv/DistanceTracker.java
@@ -10,6 +10,7 @@ import org.bytedeco.javacpp.opencv_imgproc;
 import org.bytedeco.javacpp.indexer.FloatIndexer;
 
 public class DistanceTracker {
+    private final TrackFeatures tracker = new TrackFeatures();
     private final Mat keyPoints = new Mat();
     private final Scalar color;
 
@@ -19,26 +20,34 @@ public class DistanceTracker {
         this.color = color;
     }
 
-    public void update(Mat videoImage, boolean motionDetected,
-            TrackFeatures trackFeatures) {
+    public void updateTrackerData(Mat videoImage) {
+        tracker.update(videoImage);
+    }
+
+    public void update(Mat videoImage, boolean motionDetected) {
         if (motionDetected) {
             resetTrackFeatures = true;
-        } else if (resetTrackFeatures == true) {
-            resetDistance(videoImage, trackFeatures);
+        } else if (resetTrackFeatures) {
+            resetDistance(videoImage);
         }
     }
 
-    private void resetDistance(Mat videoImage, TrackFeatures trackFeatures) {
-        trackFeatures.reset(videoImage, null);
+    private void resetDistance(Mat videoImage) {
+        tracker.reset(videoImage, null);
         resetTrackFeatures = false;
-        trackFeatures.keyPoints().copyTo(keyPoints);
+        tracker.keyPoints().copyTo(keyPoints);
     }
 
     public void reset() {
         resetTrackFeatures = true;
     }
 
-    public double distance2(Mat currentKeyPoints) {
+    public boolean hasFeatures() {
+        return tracker.haveFeatures();
+    }
+
+    public double distance2() {
+        Mat currentKeyPoints = tracker.keyPoints();
         double distance2 = 0.0;
         if (size(currentKeyPoints) > 0) {
             FloatIndexer from = keyPoints.createIndexer();
@@ -46,11 +55,8 @@ public class DistanceTracker {
             long n = Math.min(from.rows(), to.rows());
             for (int i = 0; i < n; i++) {
                 distance2 = Math.max(distance2,
-                        teaselib.core.javacv.util.Geom.distance2(
-                                new Point((int) from.get(i, 0),
-                                        (int) from.get(i, 1)),
-                                new Point((int) to.get(i, 0),
-                                        (int) to.get(i, 1))));
+                        teaselib.core.javacv.util.Geom.distance2(new Point((int) from.get(i, 0), (int) from.get(i, 1)),
+                                new Point((int) to.get(i, 0), (int) to.get(i, 1))));
             }
         }
         return distance2;
@@ -60,7 +66,9 @@ public class DistanceTracker {
         return points.rows();
     }
 
-    public void renderDebug(Mat output, Mat currentKeyPoints) {
+    public void renderDebug(Mat output) {
+        Mat currentKeyPoints = tracker.keyPoints();
+
         FloatIndexer from = keyPoints.createIndexer();
         FloatIndexer to = currentKeyPoints.createIndexer();
         long n = Math.min(from.rows(), to.rows());
@@ -71,12 +79,15 @@ public class DistanceTracker {
             p1.close();
             p2.close();
         }
-        int distance = (int) Math.sqrt(distance2(currentKeyPoints));
+        int distance = (int) Math.sqrt(distance2());
         Point p = new Point(0, output.rows() - 20);
-        putText(output, Integer.toString(distance), p, FONT_HERSHEY_PLAIN, 1.75,
-                color);
+        putText(output, Integer.toString(distance), p, FONT_HERSHEY_PLAIN, 1.75, color);
         p.close();
         from.release();
         to.release();
+    }
+
+    public void render(Mat debugOutput, Scalar color) {
+        tracker.render(debugOutput, color);
     }
 }

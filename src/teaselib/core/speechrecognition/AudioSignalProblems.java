e3eba49c04b006ebdc8ab4c47caaa5ba49cd581e
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-02-07 21:42:16 +0100

    Relaxed audio problem severity for TooQuiet - normal during speech

--- src/teaselib/core/speechrecognition/AudioSignalProblems.java
+++ src/teaselib/core/speechrecognition/AudioSignalProblems.java
@@ -2,15 +2,29 @@ package teaselib.core.speechrecognition;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import teaselib.core.speechrecognition.SpeechRecognition.AudioSignalProblem;
 
 public class AudioSignalProblems {
-    Map<SpeechRecognition.AudioSignalProblem, AtomicInteger> problems = new EnumMap<>(AudioSignalProblem.class);
+    private final Map<SpeechRecognition.AudioSignalProblem, AtomicInteger> problems = new EnumMap<>(
+            AudioSignalProblem.class);
+    private final Map<SpeechRecognition.AudioSignalProblem, Integer> limits = new EnumMap<>(AudioSignalProblem.class);
 
     public AudioSignalProblems() {
         clear();
+        setLimits();
+    }
+
+    private void setLimits() {
+        limits.put(AudioSignalProblem.None, Integer.MAX_VALUE);
+        limits.put(AudioSignalProblem.Noise, 0);
+        limits.put(AudioSignalProblem.NoSignal, 0);
+        limits.put(AudioSignalProblem.TooLoud, 0);
+        limits.put(AudioSignalProblem.TooQuiet, 1);
+        limits.put(AudioSignalProblem.TooFast, 0);
+        limits.put(AudioSignalProblem.TooSlow, 0);
     }
 
     public void clear() {
@@ -25,8 +39,8 @@ public class AudioSignalProblems {
     }
 
     public boolean occured() {
-        for (AtomicInteger value : problems.values()) {
-            if (value.get() > 0) {
+        for (Entry<AudioSignalProblem, AtomicInteger> problem : problems.entrySet()) {
+            if (problem.getValue().get() > limits.get(problem.getKey())) {
                 return true;
             }
         }

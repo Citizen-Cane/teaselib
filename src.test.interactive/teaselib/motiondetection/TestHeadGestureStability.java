5335142ce25083ac274d9e4878b71831ce7dec19
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-03-22 09:06:25 +0100

    Refactored head gesture tracking to track down system exit crash

---
+++ src.test.interactive/teaselib/motiondetection/TestHeadGestureStability.java
@@ -0,0 +1,42 @@
+package teaselib.motiondetection;
+
+import org.bytedeco.javacpp.opencv_core.Point;
+import org.junit.Test;
+
+import teaselib.core.Configuration;
+import teaselib.core.VideoRenderer.Type;
+import teaselib.core.devices.DeviceCache;
+import teaselib.core.devices.Devices;
+import teaselib.core.javacv.VideoRendererJavaCV;
+import teaselib.motiondetection.MotionDetector.MotionSensitivity;
+import teaselib.test.DebugSetup;
+
+public class TestHeadGestureStability {
+    MotionSensitivity AssumeSensitivity = MotionSensitivity.Low;
+    MotionSensitivity GestureSensitivity = MotionSensitivity.High;
+
+    @Test
+    public void testHeadGestures() throws InterruptedException {
+        Configuration config = DebugSetup.getConfiguration();
+        Devices devices = new Devices(config);
+
+        MotionDetector motionDetector = devices.get(MotionDetector.class).getDefaultDevice();
+        motionDetector.setViewPoint(ViewPoint.EyeLevel);
+
+        motionDetector.setVideoRenderer(new VideoRendererJavaCV(Type.CameraFeedback) {
+            @Override
+            protected Point getPosition(Type type, int width, int height) {
+                return new Point(0, 0);
+            }
+        });
+        // motionDetector.start();
+        DeviceCache.connect(motionDetector);
+
+        for (int i = 0; i < 1; i++) {
+            motionDetector.start();
+            Thread.sleep(1000);
+            motionDetector.stop();
+            Thread.sleep(500);
+        }
+    }
+}

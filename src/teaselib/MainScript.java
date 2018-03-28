dffb0e77d736891c0de4bd7e34322bbb0b08a474
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-03-28 14:33:03 +0200

    MainScript is a RunnableScript

--- src/teaselib/MainScript.java
+++ src/teaselib/MainScript.java
@@ -1,10 +1,12 @@
 package teaselib;
 
+import teaselib.functional.RunnableScript;
+
 /**
  * @author Citizen-Cane
  *
  *         An interface to tag a script as an entry point to a project.
  */
 @FunctionalInterface
-public interface MainScript extends Runnable {
+public interface MainScript extends RunnableScript {
 }

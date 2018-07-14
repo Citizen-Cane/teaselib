08698b50e7bace3ce20a2afdd8b77230929344bc
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2018-07-14 21:54:37 +0200

    Introduced "Tools" category

--- src/teaselib/Household.java
+++ src/teaselib/Household.java
@@ -9,6 +9,7 @@ public enum Household {
     Condoms,
     Diaper,
     Duct_Tape,
+    Dust_Pan,
     Heat_Rub,
     Hairbrush,
     Ice_Tray,
@@ -18,9 +19,11 @@ public enum Household {
     Padlocks,
     Rubber_Bands,
     Ruler,
+    Sewing_Box,
     Shoe_Lace,
     Shrinkwrap,
     Tampon,
+    Vacuum_Cleaner,
     Weight,
     Wooden_Spoon,
 
@@ -33,11 +36,12 @@ public enum Household {
         Water_Based,
     }
 
-    public static final Household[] Spanking = { Hairbrush, Ruler, Wooden_Spoon, Clothes_Pegs };
+    public static final Household[] Discipline = { Hairbrush, Ruler, Wooden_Spoon, Clothes_Pegs };
     public static final Household[] Bondage = { Padlocks, Shoe_Lace, Shrinkwrap, Duct_Tape, Leash };
-    public static final Household[] Tools = { Heat_Rub, Lube, Balloons, Bell, Condoms, Diaper, Rubber_Bands, Tampon,
+    public static final Household[] Utilities = { Heat_Rub, Lube, Balloons, Bell, Condoms, Diaper, Rubber_Bands, Tampon,
             Weight };
+    public static final Household[] Tools = { Dust_Pan, Sewing_Box, Vacuum_Cleaner };
     public static final Household[] Kitchen = { Ice_Tray, Icecubes, Candle, Cigarettes };
 
-    public static final Household[] Categories[] = { Spanking, Bondage, Tools, Kitchen };
+    public static final Household[] Categories[] = { Discipline, Bondage, Utilities, Kitchen, Tools };
 }

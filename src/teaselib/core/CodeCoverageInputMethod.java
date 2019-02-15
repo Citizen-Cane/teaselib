1123e415200fe6ed93b60aaa847fe1f4ec1c30b7
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2019-02-15 23:08:56 +0100

    Refactoring, code cleanup

--- src/teaselib/core/CodeCoverageInputMethod.java
+++ src/teaselib/core/CodeCoverageInputMethod.java
@@ -26,17 +26,16 @@ import teaselib.core.ui.Prompt;
 public class CodeCoverageInputMethod extends AbstractInputMethod implements DebugInputMethod {
     Set<InputMethod.Listener> eventListeners = new LinkedHashSet<>();
 
-    private final TimeAdvanceListener timeAdvanceListener = this::handleTimeAdvance;
     private final CheckPointListener checkPointListener = this::handleCheckPointReached;
+    private final TimeAdvanceListener timeAdvanceListener = this::handleTimeAdvance;
+
+    private final AtomicReference<Prompt> activePrompt = new AtomicReference<>();
+    private final AtomicInteger result = new AtomicInteger();
+    private final CyclicBarrier checkPointScriptFunctionFinished = new CyclicBarrier(2);
 
     public CodeCoverageInputMethod(ExecutorService executor) {
         super(executor);
     }
-
-    private final AtomicReference<Prompt> activePrompt = new AtomicReference<>();
-    private final AtomicInteger result = new AtomicInteger();
-
-    CyclicBarrier checkPointScriptFunctionFinished = new CyclicBarrier(2);
 
     @Override
     public int handleShow(Prompt prompt) throws InterruptedException {
@@ -57,49 +56,6 @@ public class CodeCoverageInputMethod extends AbstractInputMethod implements Debu
                 checkPointScriptFunctionFinished.reset();
             }
         } else {
-            // TODO causes return -1 DISMISSED but no script function
-            // java.lang.IndexOutOfBoundsException: -1-> [Gesture=Nod text='Jawohl, #title' display='Jawohl, Frau
-            // Streng']
-            // waiting result=UNDEFINED: teaselib.core.CodeCoverageInputMethod@5dcd8c7a
-            // at teaselib.core.ui.Prompt.setResultOnce(Prompt.java:105)
-            // at teaselib.core.ui.Prompt.signalResult(Prompt.java:126)
-            // at teaselib.core.ui.AbstractInputMethod.signalResult(AbstractInputMethod.java:73)
-            // at teaselib.core.ui.AbstractInputMethod.awaitAndSignalResult(AbstractInputMethod.java:62)
-            // at teaselib.core.ui.AbstractInputMethod.access$0(AbstractInputMethod.java:59)
-            // at teaselib.core.ui.AbstractInputMethod$1.call(AbstractInputMethod.java:39)
-            // at teaselib.core.ui.AbstractInputMethod$1.call(AbstractInputMethod.java:1)
-            // at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-            // at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-            // at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-            // at java.base/java.lang.Thread.run(Thread.java:834)
-            //
-            // result of java.lang.IllegalStateException: Indeterministic behavior detected -> coverage returns
-            // DISMISSED of previous/next prompt
-            // : Expected prompt[Der Sklave ist bereit, #title] but got [Der Sklaven-Saft ist ausgelaufen, #title, Bitte
-            // melken Sie mich ab, #title]
-            // at teaselib.core.CodeCoverage$1.promptShown(CodeCoverage.java:62)
-            // at teaselib.core.CodeCoverageInputMethod.lambda$4(CodeCoverageInputMethod.java:193)
-            // at java.base/java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:195)
-            // at java.base/java.util.Iterator.forEachRemaining(Iterator.java:133)
-            // at java.base/java.util.Spliterators$IteratorSpliterator.forEachRemaining(Spliterators.java:1801)
-            // at java.base/java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:484)
-            // at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:474)
-            // at java.base/java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:913)
-            // at java.base/java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)
-            // at java.base/java.util.stream.ReferencePipeline.reduce(ReferencePipeline.java:558)
-            // at teaselib.core.CodeCoverageInputMethod.firePromptShown(CodeCoverageInputMethod.java:194)
-            // at teaselib.core.CodeCoverageInputMethod.setResult(CodeCoverageInputMethod.java:77)
-            // at java.base/java.util.concurrent.atomic.AtomicReference.getAndUpdate(AtomicReference.java:187)
-            // at teaselib.core.CodeCoverageInputMethod.handleCheckPointReached(CodeCoverageInputMethod.java:66)
-            // at teaselib.core.TeaseLib.checkPointReached(TeaseLib.java:301)
-            // at teaselib.core.ScriptFutureTask.lambda$0(ScriptFutureTask.java:38)
-            // at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-            // at teaselib.core.ScriptFutureTask.run(ScriptFutureTask.java:54)
-            // at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
-            // at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
-            // at java.base/java.lang.Thread.run(Thread.java:834)
-            //
-
             return firePromptShown(activePrompt.get());
         }
     }
@@ -114,17 +70,8 @@ public class CodeCoverageInputMethod extends AbstractInputMethod implements Debu
         }
     }
 
-    private void handleTimeAdvance(TimeAdvancedEvent timeAdvancedEvent) {
+    private void handleTimeAdvance(@SuppressWarnings("unused") TimeAdvancedEvent timeAdvancedEvent) {
         // Ignore
-    }
-
-    Prompt forwardResult(Prompt prompt) {
-        if (hasScriptFunction(prompt) && resultSet()) {
-            forwardResult();
-            return null;
-        } else {
-            return prompt;
-        }
     }
 
     private static boolean hasScriptFunction(Prompt prompt) {
@@ -139,22 +86,7 @@ public class CodeCoverageInputMethod extends AbstractInputMethod implements Debu
         return result.get() > Prompt.DISMISSED;
     }
 
-    private void forwardResult() {
-        try {
-            if (resultNotSet()) {
-                throw new IllegalArgumentException("Result must be set to choice");
-            }
-            synchronized (this) {
-                if (!Thread.currentThread().isInterrupted()) {
-                    checkPointScriptFunctionFinished.await();
-                }
-            }
-        } catch (InterruptedException | BrokenBarrierException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    Prompt forwardResultAndHandleTimeout(Prompt prompt) {
+    private Prompt forwardResultAndHandleTimeout(Prompt prompt) {
         if (hasScriptFunction(prompt) && resultSet()) {
             synchronized (this) {
                 if (resultSet()) {
@@ -171,6 +103,21 @@ public class CodeCoverageInputMethod extends AbstractInputMethod implements Debu
             return null;
         } else {
             return prompt;
+        }
+    }
+
+    private void forwardResult() {
+        try {
+            if (resultNotSet()) {
+                throw new IllegalArgumentException("Result must be set to choice");
+            }
+            synchronized (this) {
+                if (!Thread.currentThread().isInterrupted()) {
+                    checkPointScriptFunctionFinished.await();
+                }
+            }
+        } catch (InterruptedException | BrokenBarrierException e) {
+            Thread.currentThread().interrupt();
         }
     }
 

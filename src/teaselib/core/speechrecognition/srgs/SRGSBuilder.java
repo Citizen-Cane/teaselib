175d115832727e96d15adcc212e7cb55d5f30861
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2019-03-29 08:51:46 +0100

    Some refactoring before adding alternative phrases

--- src/teaselib/core/speechrecognition/srgs/SRGSBuilder.java
+++ src/teaselib/core/speechrecognition/srgs/SRGSBuilder.java
@@ -28,29 +28,6 @@ public class SRGSBuilder<T> extends AbstractSRGSBuilder<T> {
         }
     }
 
-    private void createRule(Element grammar, List<Element> inventoryItems, int index, List<? extends T> speechPart) {
-        String name = ruleName(index);
-        for (int i = 0; i < speechPart.size(); i++) {
-            String id = speechPart.size() > 1 ? choiceName(index, i) : name;
-            String text = speechPart.get(i).toString();
-            Element rule = createRule(id);
-            appendText(rule, text);
-            grammar.appendChild(rule);
-        }
-
-        if (speechPart.size() == 1) {
-            for (Element element : inventoryItems) {
-                element.appendChild(ruleRef(name));
-            }
-        } else if (speechPart.size() == inventoryItems.size()) {
-            for (int i = 0; i < speechPart.size(); i++) {
-                inventoryItems.get(i).appendChild(ruleRef(choiceName(index, i)));
-            }
-        } else {
-            throw new IllegalArgumentException("Choices must be all different or all the same");
-        }
-    }
-
     private List<Element> createMainRule(Element grammar) {
         Element mainRule = createRule(MAIN_RULE_NAME);
         int max = SequenceUtil.max(choices);
@@ -64,6 +41,41 @@ public class SRGSBuilder<T> extends AbstractSRGSBuilder<T> {
         mainRule.appendChild(inventoryNode);
         grammar.appendChild(mainRule);
         return inventoryItems;
+    }
+
+    private void createRule(Element grammar, List<Element> inventoryItems, int index, List<? extends T> speechPart) {
+        String name = createRule(grammar, index, speechPart);
+        addRuleToInventory(inventoryItems, index, speechPart, name);
+    }
+
+    private String createRule(Element grammar, int index, List<? extends T> speechPart) {
+        String name = ruleName(index);
+        for (int i = 0; i < speechPart.size(); i++) {
+            String id = speechPart.size() > 1 ? choiceName(index, i) : name;
+            Element rule = createRule(id);
+            grammar.appendChild(rule);
+
+            // TODO Single text items can be added to the rule right away
+            // - alternative phrases must be enumed via <one-of> <item> ... </one-of> </item>
+            String text = speechPart.get(i).toString();
+            appendText(rule, text);
+        }
+        return name;
+    }
+
+    private void addRuleToInventory(List<Element> inventoryItems, int index, List<? extends T> speechPart,
+            String name) {
+        if (speechPart.size() == 1) {
+            for (Element element : inventoryItems) {
+                element.appendChild(ruleRef(name));
+            }
+        } else if (speechPart.size() == inventoryItems.size()) {
+            for (int i = 0; i < speechPart.size(); i++) {
+                inventoryItems.get(i).appendChild(ruleRef(choiceName(index, i)));
+            }
+        } else {
+            throw new IllegalArgumentException("Choices must be all different or all the same");
+        }
     }
 
     private static String choiceName(int index, int i) {

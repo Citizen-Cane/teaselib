716cc0668d1d8fed27f6404851c3e01a151206da
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2019-09-29 16:29:26 +0200

    Completed PhraseSliceTest to test optimal result

--- src.test/teaselib/core/speechrecognition/srgs/PhrasesSliceTest.java
+++ src.test/teaselib/core/speechrecognition/srgs/PhrasesSliceTest.java
@@ -98,19 +98,20 @@ public class PhrasesSliceTest {
         assertEquals(new PhraseStringSequences(result("Yes", 0, 1, 2), result("No", 3, 4, 5)), slice1);
 
         Sequences<PhraseString> slice2 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("Miss", 0, 3)), slice2);
+        assertEquals(new PhraseStringSequences(result("Miss of", 0, 3), result("of course", 1, 2, 4, 5)), slice2);
 
-        Sequences<PhraseString> slice3 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("of course", 0, 1, 2, 3, 4, 5)), slice3);
+        List<Sequences<PhraseString>> subOptimal = complete(choices);
+        candidates.add(subOptimal);
+        List<Sequences<PhraseString>> optimal = Sequences.reduce(candidates);
+        assertNotEquals(optimal, subOptimal);
+        assertEquals(6, Sequences.commonness(optimal));
+        assertEquals(4, Sequences.commonness(subOptimal));
 
-        Sequences<PhraseString> slice4 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("not", 3, 4, 5)), slice4);
-
-        Sequences<PhraseString> slice5 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("Miss", 1, 4)), slice5);
-
-        Sequences<PhraseString> empty = advance(choices);
-        assertTrue(empty.isEmpty());
+        assertEquals(new PhraseStringSequences(result("Yes", 0, 1, 2), result("No", 3, 4, 5)), optimal.get(0));
+        assertEquals(new PhraseStringSequences(result("Miss", 0, 3)), optimal.get(1));
+        assertEquals(new PhraseStringSequences(result("of course", 0, 1, 2, 3, 4, 5)), optimal.get(2));
+        assertEquals(new PhraseStringSequences(result("not", 3, 4, 5)), optimal.get(3));
+        assertEquals(new PhraseStringSequences(result("Miss", 1, 4)), optimal.get(4));
     }
 
     @Test
@@ -119,28 +120,19 @@ public class PhrasesSliceTest {
                 choice("Miss, of course", 0), choice("of Course, Miss", 1), choice("of course", 2),
                 choice("Miss, of course, Miss", 3));
 
-        // List<Sequences<PhraseString>> subOptimal = complete(choices);
-        // candidates.add(subOptimal);
-        // List<Sequences<PhraseString>> optimal = Sequences.reduce(candidates);
-        // assertNotEquals(optimal, subOptimal);
-        // assertEquals(3, Sequences.commonness(optimal));
-        // assertEquals(2, Sequences.commonness(subOptimal));
-
-        // Slices correctly but not optimal since only single items are moved
-        // -> this causes the next segment to be sliced disjunct,
-        // but what we want is a different slice common chunk
-        // -> move all occurrences of an element to the "without" branch
         Sequences<PhraseString> slice1 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("Miss", 0, 3)), slice1);
+        assertEquals(new PhraseStringSequences(result("Miss of course", 0, 3), result("of course", 1, 2)), slice1);
 
-        Sequences<PhraseString> slice2 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("of course", 0, 1, 2, 3)), slice2);
+        List<Sequences<PhraseString>> subOptimal = complete(choices);
+        candidates.add(subOptimal);
+        List<Sequences<PhraseString>> optimal = Sequences.reduce(candidates);
+        assertNotEquals(optimal, subOptimal);
+        assertEquals(4, Sequences.commonness(optimal));
+        assertEquals(2, Sequences.commonness(subOptimal));
 
-        Sequences<PhraseString> slice3 = advance(choices);
-        assertEquals(new PhraseStringSequences(result("Miss", 1, 3)), slice3);
-
-        Sequences<PhraseString> empty = advance(choices);
-        assertTrue(empty.isEmpty());
+        assertEquals(new PhraseStringSequences(result("Miss", 0, 3)), optimal.get(0));
+        assertEquals(new PhraseStringSequences(result("of course", 0, 1, 2, 3)), optimal.get(1));
+        assertEquals(new PhraseStringSequences(result("Miss", 1, 3)), optimal.get(2));
     }
 
     @Test

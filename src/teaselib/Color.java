1e71cb2e0c92ecb8ba3c6b121a48e6028c8f42b8
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2020-09-22 11:15:24 +0200

    Completed color tags for detailed item descriptions

--- src/teaselib/Color.java
+++ src/teaselib/Color.java
@@ -1,5 +1,23 @@
 package teaselib;
 
+/**
+ * @author Citizen Cane
+ * 
+ *         Color tags for items: use it in scripts to describe items in more detail.
+ *         <p>
+ *         Only basic/typical colors are listed here, to limit the available choices.
+ *
+ */
 public enum Color {
     Black,
+    Brown,
+    Blue,
+    Green,
+    Grey,
+    Orange,
+    Pink,
+    Red,
+    Violett,
+    White,
+    Yellow
 }

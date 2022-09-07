a83701bc4034306e1240f22fc0a1131b11a9e194
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2022-09-07 22:37:07 +0200

    Changed java sound.spi to com.tagtraum.ffsampledsp-complete

--- src.test/teaselib/core/sound/AudioDevicesTest.java
+++ src.test/teaselib/core/sound/AudioDevicesTest.java
@@ -1,10 +1,14 @@
 package teaselib.core.sound;
 
+import static org.junit.Assume.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.stream.Stream;
 
+import javax.sound.sampled.AudioFileFormat.Type;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -14,37 +18,40 @@ import org.junit.Test;
 public class AudioDevicesTest {
 
     @Test
-    public void testFormats() {
-
-        var formats = AudioDevices.formats();
+    public void testAudioSystem() {
+        var formats = Stream.of(AudioSystem.getAudioFileTypes()).map(Type::toString).toList();
         assertNotNull(formats);
-        assertNotEquals(0, formats.size());
-
+        assertEquals(3, formats.size());
         assertTrue(formats.contains("WAVE"));
         assertTrue(formats.contains("AU"));
         assertTrue(formats.contains("AIFF"));
 
-        assertTrue(formats.contains("AIFF-C"), "part of mp3spi");
-
-        // TODO with mp3spi there's no mp3 file type
-        assertTrue(formats.contains("MP3"), "MP3 spi");
-        assertTrue(formats.contains("OGG"), "Ogg spi");
+        var lines = AudioDevices.inputs();
+        assumeTrue(lines.size() > 0);
     }
+
+    // Audio-Service-Provider:
+    // https://www.tagtraum.com/ffsampledsp/
+    // https://www.tagtraum.com/ffmpeg/
 
     @Test
     public void testReadMp3SpeechShort() throws IOException, UnsupportedAudioFileException {
-        InputStream mp3 = getClass().getResource("0.mp3").openStream();
-        assertNotNull(mp3);
-        AudioInputStream in = AudioSystem.getAudioInputStream(mp3);
-        assertNotNull(in.getFormat());
+        testMp3("0.mp3");
     }
 
     @Test
     public void testReadMp3SpeechLong() throws IOException, UnsupportedAudioFileException {
-        InputStream mp3 = getClass().getResource("1.mp3").openStream();
+        testMp3("1.mp3");
+    }
+
+    private void testMp3(String resource) throws UnsupportedAudioFileException, IOException {
+        InputStream mp3 = getClass().getResource(resource).openStream();
         assertNotNull(mp3);
         AudioInputStream in = AudioSystem.getAudioInputStream(mp3);
-        assertNotNull(in.getFormat());
+        AudioFormat format = in.getFormat();
+        assertNotNull(format);
+        assertEquals(1, format.getChannels());
+        assertEquals(32000, format.getSampleRate());
     }
 
 }
--- src/teaselib/core/sound/AudioDevices.java
+++ src/teaselib/core/sound/AudioDevices.java
@@ -1,11 +1,20 @@
 package teaselib.core.sound;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
-import javax.sound.sampled.AudioFileFormat.Type;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.BooleanControl;
+import javax.sound.sampled.CompoundControl;
+import javax.sound.sampled.Control;
+import javax.sound.sampled.EnumControl;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Port;
 
 /**
  * @author Citizen-Cane
@@ -14,15 +23,93 @@ import javax.sound.sampled.AudioSystem;
 public class AudioDevices {
 
     public static List<String> inputs() {
-        return Collections.emptyList();
+        Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+        List<Line.Info> availablePorts = new ArrayList<>();
+        List<Line.Info> availableLines = new ArrayList<>();
+
+        for (Mixer.Info mixerInfo : mixers) {
+            Mixer mixer = AudioSystem.getMixer(mixerInfo);
+            System.out.println("\nFound " + mixer.getClass().getSimpleName() + ": " + "\"" + mixerInfo.getName() + "\""
+                    + " (\"" + mixerInfo.getDescription() + "\", " + mixerInfo.getVendor() + ", " + mixerInfo.getVersion() + ")");
+            try {
+                mixer.open();
+                for (Line.Info lineInfo : mixer.getSourceLineInfo()) {
+                    Line line = mixer.getLine(lineInfo);
+                    System.out.println(
+                            "    Found source " + line.getClass().getSimpleName() + ": " + lineInfo + " with " + mixer.getMaxLines(lineInfo)
+                                    + " max-instances.");
+                    try {
+                        line.open();
+                        for (Control thisControl : line.getControls()) {
+                            System.out.println(AnalyzeControl(thisControl));
+                        }
+                        if (lineInfo.matches(Port.Info.MICROPHONE) || lineInfo.matches(Port.Info.LINE_IN) || lineInfo.matches(Port.Info.COMPACT_DISC)) {
+                            availablePorts.add(lineInfo);
+                        } else {
+                            availableLines.add(lineInfo);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("\t\tLine unavailable: " + e.getMessage());
+                    }
+                }
+
+                for (Line.Info lineInfo : mixer.getTargetLineInfo()) {
+                    Line line = mixer.getLine(lineInfo);
+                    System.out.println(
+                            "    Found target " + line.getClass().getSimpleName() + ": " + lineInfo + " with " + mixer.getMaxLines(lineInfo)
+                                    + " max-instances.");
+                    try {
+                        line.open();
+                        for (Control thisControl : line.getControls()) {
+                            System.out.println(AnalyzeControl(thisControl));
+                        }
+                        if (lineInfo.matches(Port.Info.HEADPHONE) || lineInfo.matches(Port.Info.SPEAKER) || lineInfo.matches(Port.Info.LINE_OUT)) {
+                            availablePorts.add(lineInfo);
+                        } else {
+                            availableLines.add(lineInfo);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("\t\tLine unavailable: " + e.getMessage());
+                    }
+                }
+            } catch (LineUnavailableException e) {
+                System.out.println("\t\tLine unavailable." + e.getMessage());
+            }
+        }
+
+        System.out.println("\nAvailable ports:\n" + availablePorts.stream().map(Line.Info::toString).collect(Collectors.joining("\n")));
+        System.out.println("\nAvailable lines:\n" + availableLines.stream().map(Line.Info::toString).collect(Collectors.joining("\n")));
+        return availableLines.stream().map(Line.Info::toString).toList();
+
+    }
+
+    public static String AnalyzeControl(Control thisControl) {
+        String type = thisControl.getType().toString();
+        if (thisControl instanceof BooleanControl) {
+            return "\t\t    Control: " + type + " (boolean)";
+        }
+        if (thisControl instanceof CompoundControl) {
+            System.out.println("\t\t    Control: " + type +
+                    " (compound - values below)");
+            String toReturn = "";
+            for (Control children : ((CompoundControl) thisControl).getMemberControls()) {
+                toReturn += "  " + AnalyzeControl(children) + "\n";
+            }
+            return toReturn.substring(0, toReturn.length() - 1);
+        }
+        if (thisControl instanceof EnumControl) {
+            return "\t\t    Control:" + type + " (enum: " + thisControl.toString() + ")";
+        }
+        if (thisControl instanceof FloatControl) {
+            return "\t\t    Control: " + type + " (float: from " +
+                    ((FloatControl) thisControl).getMinimum() + " to " +
+                    ((FloatControl) thisControl).getMaximum() + ")";
+        }
+        return "    Control: unknown type";
     }
 
     public static List<String> outputs() {
         return Collections.emptyList();
-    }
-
-    public static List<String> formats() {
-        return Stream.of(AudioSystem.getAudioFileTypes()).map(Type::toString).toList();
     }
 
 }
